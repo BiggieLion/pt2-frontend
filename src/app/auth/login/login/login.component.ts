c4b663a87705b5e10e534ebfb0bbb9f440c7b755
Enrique Javet Sanchez Cerna <esanchez1806@alumno.ipn.mx>
2024-10-10 09:30:36 -0600

    Add Axios and petition Login

--- src/app/auth/login/login/login.component.ts
+++ src/app/auth/login/login/login.component.ts
@@ -9,7 +9,10 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import { Router, RouterModule } from '@angular/router';
 import { TopMenuComponent } from '../../../misc/topMenu/top-menu/top-menu.component';
 import { LocalStorageService } from 'angular-web-storage'; // Importa el servicio de almacenamiento local
-
+import axios from 'axios';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -23,7 +26,7 @@ import { LocalStorageService } from 'angular-web-storage'; // Importa el servici
 export class LoginComponent {
   validateForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router, private localStorage: LocalStorageService) { 
+  constructor(private fb: FormBuilder, private router: Router, private localStorage: LocalStorageService,private http: HttpClient) { 
     this.validateForm = this.fb.group({
       userName: ['', [Validators.required]],
       password: ['', [Validators.required]],
@@ -34,14 +37,15 @@ export class LoginComponent {
   submitForm(): void {
     if (this.validateForm.valid) {
       const { userName, password } = this.validateForm.value;
+      this.login(userName, password)
       if (userName === 'risky@mail.com' && password === 'test123') {
         console.log('submit', this.validateForm.value);
         this.localStorage.set('typeUser', 'normal'); 
-        this.router.navigate(['/dashboard']);
+        //this.router.navigate(['/dashboard']);
       } else if (userName === 'radmin@mail.com' && password === 'admin123') {
         console.log('submit', this.validateForm.value);
         this.localStorage.set('typeUser', 'admin'); 
-        this.router.navigate(['/dashboard']);
+        //this.router.navigate(['/dashboard']);
       } else {
         console.log('Datos erróneos');
       }
@@ -57,5 +61,20 @@ export class LoginComponent {
 
   navigateToSignUp(): void {
     this.router.navigate(['/sign-up']);
+  }
+
+  async login(email:string, password:string) {
+    try {
+      let url = 'http://127.0.0.1:3000/api/v1/auth/login';
+      // const response = await axios.get(url);
+      const response = await axios.post( url , {
+        "email": email,
+        "password": password  
+      });
+      console.log("RESPONSE");
+      console.log(response)
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
   }
 }
--- src/app/details-page/details-page.component.ts
+++ src/app/details-page/details-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../misc/navBar/nav-bar/nav-bar.component'; 
+import axios from 'axios';
 
 @Component({
   selector: 'app-details-page',
@@ -11,6 +12,17 @@ import { NavBarComponent } from '../misc/navBar/nav-bar/nav-bar.component';
 export class DetailsPageComponent {
   ngOnInit(){
     
+  }
+  async login() {
+    try {
+      const response = await axios.post('http://localhost:3000/api/v1/auth/login', {
+        email: "usiel@gmail.com",
+        password: "password91"
+      });
+      console.log('Login successful:', response.data);
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
   }
 
 }
